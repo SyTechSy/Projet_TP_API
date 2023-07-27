63bd9b004b7d4348d15bfe589079a2d7c75f8e1c
unknown <dick.aichata@odc.local>
2023-07-27 10:21:57 +0000

    commit aicha quiz

--- src/main/java/com/odk3/projet_tp_api/Controller/QuizController.java
+++ src/main/java/com/odk3/projet_tp_api/Controller/QuizController.java
@@ -1,8 +1,11 @@
 package com.odk3.projet_tp_api.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QuizController {
+
 }
--- src/main/java/com/odk3/projet_tp_api/Service/QuizService.java
+++ src/main/java/com/odk3/projet_tp_api/Service/QuizService.java
@@ -1,7 +1,54 @@
 package com.odk3.projet_tp_api.Service;
 
+import com.odk3.projet_tp_api.Repository.QuizRepository;
+import com.odk3.projet_tp_api.model.Quiz;
+import com.odk3.projet_tp_api.model.Utilisateur;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuizService {
+
+    @Autowired
+    QuizRepository quizRepository;
+
+
+    // creation Quiz
+    public Quiz creerquiz (Quiz quiz){
+        if (quizRepository.findByIdQuiz(quiz.getIdQuiz()) ==null){
+            quizRepository .save(quiz);
+            return quizRepository.findByIdQuiz(quiz.getIdQuiz());
+        } else {
+            return null;
+        }
+    }
+// modification  de  quiz
+    public Quiz modifquiz(Quiz quiz){
+        if (quizRepository.findByIdQuiz(quiz.getIdQuiz()) != null){
+            quizRepository.save(quiz);
+            return quizRepository.findByIdQuiz(quiz.getIdQuiz());
+        }
+        else {
+            return null;
+        }
+
+    }
+    // lister le quiz
+      public List<Quiz> listQuiz() {
+        return quizRepository.findAll();
+    }
+    // supprimer le quiz
+    public String supprimeQuiz(Quiz quiz) {
+
+        if (quizRepository.findByIdQuiz(quiz.getIdQuiz()) != null) {
+            quizRepository.delete(quiz);
+            return "quiz supprimer avec succès";
+        } else {
+            return "pas de quiz ";
+        }
+
+    }
+
 }
